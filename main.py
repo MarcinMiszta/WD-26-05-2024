f5162659e857a994d64b66fd8b6268b5860400a0
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-05-26 17:45:35 +0200

    2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from numpy import *
+import pandas as pd
 
 # a = array([20, 30, 40, 50])
 # b = arange(4)
@@ -61,23 +62,94 @@ from numpy import *
 # for b in a.flat:
 #     print(b)
 
-a = arange(6)
-print(a)
-print(a.shape)
-print("")
-b = a.reshape((2, 3))
-print(b)
-print(b.shape)
-print("")
-c = b.reshape((3, 2))
-print(c)
-print(c.shape)
-print("")
-d = c.ravel()
-print(d)
-print(d.shape)
-print("")
-d = c.ravel()
-print(d)
-print(d.shape)
-print("")
+# a = arange(6)
+# print(a)
+# print(a.shape)
+# print("")
+# b = a.reshape((2, 3))
+# print(b)
+# print(b.shape)
+# print("")
+# c = b.reshape((3, 2))
+# print(c)
+# print(c.shape)
+# print("")
+# d = c.ravel()
+# print(d)
+# print(d.shape)
+# print("")
+# d = c.ravel()
+# print(d)
+# print(d.shape)
+# print("")
+
+s = pd.Series([1, 3, 5, nan, 6, 8])
+print(s)
+s = pd.Series([1, 20, 3, 40], index=['a', 'b', 'c', 'd'])
+print(s)
+data = {'kraj': ['belgia', 'polska', 'uk'], 'stolica': ['bruksela', 'warszawa', 'londyn'],
+        'populacja': [111908462, 400000000, 511908462]}
+df = pd.DataFrame(data)
+print(df)
+print(df.dtypes)
+# df = pd.read_csv('dane.csv', header=0, sep=';', decimal='.')
+# print(df)
+# df.to_csv('plik.csv', index=False)
+
+# print(s['c'])
+# print(s.c)
+# print(df[0:1])
+# print('')
+# print(df['populacja'])
+# print(df.iloc[0, 0])
+# print(df.loc[0, 'kraj'])
+# print(df.at[0, 'kraj'])
+# print('kraj: ' + df.kraj)
+# print(df.sample())
+#
+# print(df.sample(2))
+# print(df.sample(frac=0.5))
+# print(df.sample(n=10, replace=True))
+# print(df.head())
+# print(df.head(2))
+# print(df.tail(1))
+# print(df.describe())
+# print(df.T)
+
+# print(s[(s > 9)])
+# print(s.where((s >= 10)))
+# print(s.where(s > 10, 'za duze'))
+# seria = s.copy()
+# seria.where((s >= 10), 'za duze', inplace=True)
+# print('#########')
+# print(seria)
+
+print(s[~(s > 10)])
+print(s[(s < 13) & (s > 8)])
+
+print(df[(df.populacja > 1000000) & (df.index.isin([0, 2]))])
+
+print('###########')
+szukaj = ['belgia', 'polska']
+print(df.isin(szukaj))
+
+s['e'] = 15
+print(s.e)
+s['f'] = 16
+print(s)
+
+df.loc[3] = 'dodane'
+print(df)
+df.loc[4] = ['Polska', 'Warszawa', 38000000]
+print(df)
+
+new_df = df.drop([1])
+print(new_df)
+
+df.drop([3],inplace=True)
+print(df)
+
+df.drop('kraj',axis=1,inplace=True)
+df['kontynent']=['europa','azja','ameryka poludniowa','europa']
+print(df)
+
